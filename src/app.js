e454e5e9d6a09d70479c283ef4a203969172d1a2
sherifsani~ <sanisherif838@gmail.com>
2023-02-17 07:46:04 +0100

    completed destinaation page

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const menuBtn = document.querySelector(".menu");
 const closeMenuBtn = document.querySelector(".close-icon");
 const mobileNavExpanded = document.querySelector(".menu-items-mobile");
 const a = document.querySelectorAll("nav a");
+const navItemsDest = document.querySelectorAll(".destination-nav ul li");
+const destHead = document.querySelector(".dest-heading");
+const destDesc = document.querySelector(".dest-desc p");
+const destImg = document.querySelector(".dest-img img");
+const destDistance = document.querySelector(".distance p");
+const destTime = document.querySelector(".time p");
 
 // mobile navbar display
 menuBtn.addEventListener("click", () => {
@@ -17,3 +23,29 @@ a.forEach((link) => {
     link.setAttribute("aria-current", "page");
   }
 });
+// active destination nav
+navItemsDest.forEach((item) => {
+  item.addEventListener("click", function () {
+    item.classList.add("active");
+    navItemsDest.forEach((a) => {
+      if (a !== item) a.classList.remove("active");
+    });
+  });
+});
+// fetching data from data.json
+async function fetchData() {
+  let response = await fetch("../src/data.json");
+  let data = await response.json();
+  const destinationData = data.destinations;
+
+  navItemsDest.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      destDesc.textContent = destinationData[index].description;
+      destHead.innerText = destinationData[index].name;
+      destImg.src = destinationData[index].images.png;
+      destDistance.textContent = destinationData[index].distance;
+      destTime.textContent = destinationData[index].travel;
+    });
+  });
+}
+fetchData();
